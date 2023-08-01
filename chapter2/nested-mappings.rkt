bb6a922ff267ee3e6f252f32cfbb0b9fba61b245
Julian Gindi <julian@gindi.io>
2023-08-01 14:05:27 -0700

    saving state for now again

--- chapter2/nested-mappings.rkt
+++ chapter2/nested-mappings.rkt
@@ -1,77 +1,24 @@
 #lang sicp
 
-(define (square n)
-  (* n n))
+(#%require "utils.rkt")
 
-(define (smallest-divisor n)
-  (find-divisor n 2))
-
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n)
-         n)
-        ((divides? test-divisor n)
-         test-divisor)
-        (else (find-divisor
-               n
-               (+ test-divisor 1)))))
-
-(define (divides? a b)
-  (= (remainder b a) 0))
-
-(define (prime? n)
-  (= n (smallest-divisor n)))
-  
-
-(define (accumulate op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op 
-                      initial 
-                      (cdr sequence)))))
-
-(define (filter predicate sequence)
-  (cond ((null? sequence) nil)
-        ((predicate (car sequence))
-         (cons (car sequence)
-               (filter predicate 
-                       (cdr sequence))))
-        (else  (filter predicate 
-                       (cdr sequence)))))
-
-(define (flatmap proc seq)
-  (accumulate append nil (map proc seq)))
-
-(define (enumerate-interval start end)
-  (if (> start end)
-      nil
-      (cons start (enumerate-interval (+ start 1) end))))
-
-(define (get-nums n)
+(define (unique-pairs n)
   (flatmap (lambda (i)
              (map (lambda (j)
                     (list i j))
                   (enumerate-interval 1 (- i 1))))
            (enumerate-interval 1 n)))
 
-(define (prime-sum? pair)
-  (prime? (+ (car pair) (cadr pair))))
+(define (prime-sum? nums)
+  (prime? (apply + nums)))
 
 (define (make-pair-sum pair)
   (list (car pair)
         (cadr pair)
         (+ (car pair) (cadr pair))))
 
-
 (define (prime-sum-pairs n)
   (map make-pair-sum
        (filter
         prime-sum?
-        (flatmap
-         (lambda (i)
-           (map (lambda (j)
-                  (list i j))
-                (enumerate-interval
-                 1
-                 (- i 1))))
-         (enumerate-interval 1 n)))))
+        (unique-pairs n))))
