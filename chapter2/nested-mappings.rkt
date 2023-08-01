028b76df460cb446648e92134d9464c085d1e9ec
Julian Gindi <julian@gindi.io>
2023-08-01 16:05:08 -0700

    unique triples

--- chapter2/nested-mappings.rkt
+++ chapter2/nested-mappings.rkt
@@ -9,6 +9,21 @@
                   (enumerate-interval 1 (- i 1))))
            (enumerate-interval 1 n)))
 
+(define (unique-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda (j)
+                        (map (lambda (k)
+                               (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                      (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (t n s)
+  (filter (lambda (x)
+            (<= (apply + x) s))
+          (unique-triples n)))
+
+
 (define (prime-sum? nums)
   (prime? (apply + nums)))
 
@@ -22,3 +37,5 @@
        (filter
         prime-sum?
         (unique-pairs n))))
+
+(t 10 100)
