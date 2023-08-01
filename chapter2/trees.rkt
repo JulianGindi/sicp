a583b0af48fe4a9ac5f1cba003bb2a8d980e3722
Julian Gindi <julian@gindi.io>
2023-07-31 17:26:49 -0700

    more subsets and trees

--- chapter2/trees.rkt
+++ chapter2/trees.rkt
@@ -31,8 +31,42 @@
         (else ; we have a pair
          (append (fringe (car t)) (fringe (cdr t))))))
 
+(define (scale-tree tree factor)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree factor))
+        (else
+         (cons (scale-tree (car tree)
+                           factor)
+               (scale-tree (cdr tree)
+                           factor)))))
 
+(define (square n)
+  (* n n))
 
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else
+         (cons (square-tree (car tree)) (square-tree (cdr tree))))))
 
+(define (msquare-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (msquare-tree sub-tree)
+             (square sub-tree)))
+       tree))
 
+(define (tree-map op tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (op tree))
+        (else
+         (cons (tree-map op (car tree)) (tree-map op (cdr tree))))))
 
+(define (tmsquare-tree tree)
+  (tree-map square tree))
+
+(tmsquare-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;(1 (4 (9 16) 25) (36 49))
